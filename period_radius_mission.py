8e35586b080d2d8033f2dbfe19e6e325da42f315
Ethan Kruse <6944089+ethankruse@users.noreply.github.com>
2020-02-29 21:05:19 -0500

    fixing up the confirmed period/radius plot

--- period_radius_mission.py
+++ period_radius_mission.py
@@ -1,12 +1,18 @@
 import pandas as pd
 from bokeh import plotting
-from bokeh import palettes
 from bokeh.themes import Theme
 from bokeh.io import curdoc
-from bokeh.models import NumeralTickFormatter
+from bokeh.models import NumeralTickFormatter, OpenURL, TapTool
+import numpy as np
 
 theme = Theme(filename="./exoplots_theme.yaml")
 curdoc().theme = theme
+
+# colorblind friendly palette from https://personal.sron.nl/~pault/
+# other ideas: https://thenode.biologists.com/data-visualization-with-flying-colors/research/
+colors = ['#228833', '#ee6677', '#66ccee', '#aa3377', '#ccbb44', '#4477aa', 
+          '#aaaaaa']
+markers = ['circle', 'square', 'triangle', 'diamond', 'inverted_triangle']
 
 
 datafile = 'data/confirmed-planets.csv'
@@ -26,26 +32,42 @@ TOOLTIPS = [
 ]
 
 fig = plotting.figure(x_axis_type='log', y_axis_type='log', tooltips=TOOLTIPS)
+fig.add_tools(TapTool())
 
 
-missions = ['Kepler', 'K2', 'TESS']
-colors = palettes.Category10[len(missions)]
+missions = ['Kepler', 'K2', 'TESS', 'other']
+# colors = palettes.Category10[len(missions)]
 
 for ii, imiss in enumerate(missions):
-    good = df['pl_facility'] == imiss
+    if imiss == 'other':
+        good = df['pl_tranflag'].astype(bool) & ~np.in1d(df['pl_facility'], missions)
+        imiss = 'Ground'
+    else:
+        good = df['pl_facility'] == imiss
+    
+    alpha = 1. - good.sum()/1000.
+    alpha = max(0.5, alpha)
     
     source = plotting.ColumnDataSource(data=dict(
     planet=df['pl_name'][good],
     period=df['pl_orbper'][good],
     radius=df['pl_rade'][good],
+    host=df['pl_hostname'][good]
     ))
     
     
-    fig.circle('period', 'radius', color=colors[ii], source=source, size=8,
-               legend_label=imiss, muted_alpha=0.2, muted_color=colors[ii])
+    glyph = fig.scatter('period', 'radius', color=colors[ii], source=source, size=8,
+               legend_label=imiss, muted_alpha=0.1, muted_color=colors[ii],
+               alpha=alpha, marker=markers[ii], nonselection_alpha=alpha,
+               nonselection_color=colors[ii])
 
 
-fig.yaxis.axis_label = 'Radius (Earth)'
+url = "https://exoplanetarchive.ipac.caltech.edu/overview/@host"
+taptool = fig.select(TapTool)
+taptool.callback = OpenURL(url=url)
+
+
+fig.yaxis.axis_label = 'Radius (Earth Radii)'
 fig.yaxis.formatter = NumeralTickFormatter(format='0,0[.]00')
 
 fig.xaxis.axis_label = 'Period (days)'
@@ -58,5 +80,5 @@ fig.title.text = 'Confirmed Transiting Planets'
 
 
 
-plotting.show(fig)
+plotting.save(fig)
 
